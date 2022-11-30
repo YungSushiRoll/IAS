5a75ceff26626f112e86788087c2ce878011e5dd
Joshua Gossett <j.gosset1@gmail.com>
2022-11-30 13:05:41 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// $(document).ready(()=>{
+//     $('#4115v').on('click', ()=>{
+//         $('fieldset').hide();
+//         $('#hiddenMsg').show();
+//     });
+// });
+$(document).ready(function(){
+
+    $('.featBtnAction').on('click', function(){
+
+        if ($(this).text().includes("↓ PRODUCT")){
+            $(this).html("&uarr; PRODUCT DETAILS &uarr;")
+        } else {
+            $(this).html("&darr; PRODUCT DETAILS &darr;")
+        }
+        
+    })
+    $('.advBtn').on('click', function(){
+
+        if($(this).text().includes("↓")){
+            $(this).html("&uarr; ADVANTAGES OVER 1-WAY &uarr;")
+        } else {
+            $(this).html("&darr; ADVANTAGES OVER 1-WAY &darr;")
+        }
+        
+    })
+});
